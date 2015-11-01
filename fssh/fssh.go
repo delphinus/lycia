3ddfafe258ef2d39847823e081d10ff99163e4c1
delphinus <delphinus@remora.cx>
2015-11-01 21:04:35 +0900

    Fix parsing logic for name & args

--- fssh/fssh.go
+++ fssh/fssh.go
@@ -3,6 +3,7 @@ package fssh
 import (
 	"os"
 	"os/exec"
+	"strings"
 )
 
 var fsshENV = map[string]string{
@@ -15,15 +16,16 @@ var IsFssh = fsshENV["port"] != ""
 
 func Command(name string, args ...string) *exec.Cmd {
 	if IsFssh {
-		args = append([]string{
-			"-p", fsshENV["LC_FSSH_PORT"],
-			"-l", fsshENV["LC_FSSH_USER"],
-			fsshENV["LC_FSSH_COPY_ARGS"],
-			"localhost",
-			"PATH=" + fsshENV["LC_FSSH_PATH"],
-			name,
-		}, args...)
+		commandToExecute := strings.Join(append([]string{name}, args...), " ")
 		name = "ssh"
+		args = []string{
+			"-p", fsshENV["port"],
+			"-l", fsshENV["user"],
+			fsshENV["copy_args"],
+			"localhost",
+			"PATH=" + fsshENV["path"],
+			commandToExecute,
+		}
 	}
 	return exec.Command(name, args...)
 }
