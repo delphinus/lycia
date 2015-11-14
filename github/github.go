6643228e39e90ed0b8c01cdf382817ce2ef5b993
delphinus <delphinus@remora.cx>
2015-11-14 16:42:29 +0900

    Detect pullrequest for the branch

--- github/github.go
+++ github/github.go
@@ -1,9 +1,13 @@
 package github
 
 import (
+	"encoding/json"
 	"fmt"
+	. "github.com/delphinus35/lycia/error"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type repository struct {
@@ -16,16 +20,39 @@ func Repository(repoURL url.URL) *repository {
 
 func (repo *repository) PullrequestURL(branch string) (prURL *url.URL, err error) {
 	values := url.Values{}
-	queryString := fmt.Sprintf("repo:%s type:pr is:open head:%s", path, branch)
+	repoPath := strings.TrimLeft(repo.URL.Path, "/")
+	queryString := fmt.Sprintf("repo:%s type:pr is:open head:%s", repoPath, branch)
 	values.Add("q", queryString)
 
-	searchURL := fmt.Sprintf("%s/search/issues?%s", repo.URL, values.Encode())
+	searchURL := fmt.Sprintf("https://api.github.com/search/issues?%s", values.Encode())
 	res, err := http.Get(searchURL)
 	if err != nil {
-		err = GitUrlError("failed to fetch: " + searchURL)
+		err = LyciaError("failed to fetch: " + searchURL)
 		return
 	}
 
 	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		err = LyciaError("failed to read body: " + searchURL)
+		return
+	}
+
+	var searchIssue SearchIssue
+	if err = json.Unmarshal(body, &searchIssue); err != nil {
+		return
+	}
+
+	items := searchIssue.Items
+	if len(items) == 0 {
+		err = LyciaError("pullrequest not found")
+		return
+	}
+
+	prURL, err = url.Parse(items[0].HtmlUrl)
+	if err != nil {
+		err = LyciaError(fmt.Sprintf("html_url is invalid: %s", items[0].HtmlUrl))
+	}
 	return
 }
